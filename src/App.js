0e717b0694209775d8ae860164e69cf2959fa857
ajaythopate <ajaythopate999@gmail.com>
2023-09-10 10:49:13 +0530

    number input added

--- src/App.js
+++ src/App.js
@@ -65,6 +65,18 @@ function App() {
             />
             <label>Length: {length}</label>
           </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
         
       </div>
 
